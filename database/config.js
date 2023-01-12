f2564b58764c5d8884970ad22e79a990e38c0ab0
David <davojh27@gmail.com>
2023-01-12 09:55:29 -0500

    finalizando seccion 8

---
+++ database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const dbConnection = async() => {
+
+    try{
+        await mongoose.connect( process.env.DB_CNN );
+        mongoose.set('strictQuery', true);
+
+        console.log('DB online')
+    } catch (error){
+        console.log(error);
+        throw new Error('Error en la base de datos - Hable ocn el admin')
+    }
+    
+}
+
+
+module.exports = {
+    dbConnection
+}
